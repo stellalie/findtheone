3d7aecdfeae5704fb3c7299dd5612a2de5ae55d8
Stella Lie <ask@stellalie.com>
2013-05-28 02:02:58 +1000

    enable gii and mysql database, install yii extension yii-user

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'Find The One',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -16,18 +16,40 @@ return array(
 	'import'=>array(
 		'application.models.*',
 		'application.components.*',
+        'application.modules.user.models.*',
+        'application.modules.user.components.*',
 	),
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'Fto123!!',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
-		*/
+        'user'=>array(
+            # encrypting method (php hash function)
+            'hash' => 'md5',
+            # send activation email
+            'sendActivationMail' => true,
+            # allow access for non-activated users
+            'loginNotActiv' => false,
+            # activate user on registration (only sendActivationMail = false)
+            'activeAfterRegister' => false,
+            # automatically login from registration
+            'autoLogin' => true,
+            # registration path
+            'registrationUrl' => array('/user/registration'),
+            # recovery password path
+            'recoveryUrl' => array('/user/recovery'),
+            # login form path
+            'loginUrl' => array('/user/login'),
+            # page after login
+            'returnUrl' => array('/user/profile'),
+            # page after logout
+            'returnLogoutUrl' => array('/user/login'),
+        ),
 	),
 
 	// application components
@@ -35,6 +57,7 @@ return array(
 		'user'=>array(
 			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
+            'class' => 'WebUser',
 		),
 		// uncomment the following to enable URLs in path-format
 		/*
@@ -47,19 +70,14 @@ return array(
 			),
 		),
 		*/
-		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
-		// uncomment the following to use a MySQL database
-		/*
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
-			'charset' => 'utf8',
-		),
-		*/
+        'db'=>array(
+            'connectionString' => 'mysql:host=localhost;dbname=findtheone',
+            'emulatePrepare' => true,
+            'username' => 'root',
+            'password' => 'root',
+            'charset' => 'utf8',
+            'tablePrefix' => 'tbl_',
+        ),
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
--- protected/config/test.php
+++ protected/config/test.php
@@ -7,11 +7,14 @@ return CMap::mergeArray(
 			'fixture'=>array(
 				'class'=>'system.test.CDbFixtureManager',
 			),
-			/* uncomment the following to provide test database connection
-			'db'=>array(
-				'connectionString'=>'DSN for test database',
-			),
-			*/
+            'db'=>array(
+                'connectionString' => 'mysql:host=localhost;dbname=findtheone',
+                'emulatePrepare' => true,
+                'username' => 'root',
+                'password' => 'root',
+                'charset' => 'utf8',
+                'tablePrefix' => 'tbl_',
+            ),
 		),
 	)
 );
