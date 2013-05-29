090ab104084879286435f5307223e5429f7a6d88
Stella Lie <ask@stellalie.com>
2013-05-29 22:57:44 +1000

    changed gii password

--- protected/config/main.php
+++ protected/config/main.php
@@ -24,7 +24,7 @@ return array(
 		// uncomment the following to enable the Gii tool
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Fto123!!',
+			'password'=>'fto123',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
