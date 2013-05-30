fa77bf96884ba4c4fa39ad66b1a9ac834718f842
Stella Lie <ask@stellalie.com>
2013-05-30 13:15:14 +1000

    disable captcha functionality on registration

--- protected/modules/user/models/RegistrationForm.php
+++ protected/modules/user/models/RegistrationForm.php
@@ -19,10 +19,7 @@ class RegistrationForm extends User {
 			//array('verifyPassword', 'compare', 'compareAttribute'=>'password', 'message' => UserModule::t("Retype Password is incorrect.")),
 			array('username', 'match', 'pattern' => '/^[A-Za-z0-9_]+$/u','message' => UserModule::t("Incorrect symbols (A-z0-9).")),
 		);
-		if (!(isset($_POST['ajax']) && $_POST['ajax']==='registration-form')) {
-			array_push($rules,array('verifyCode', 'captcha', 'allowEmpty'=>!UserModule::doCaptcha('registration')));
-		}
-		
+
 		array_push($rules,array('verifyPassword', 'compare', 'compareAttribute'=>'password', 'message' => UserModule::t("Retype Password is incorrect.")));
 		return $rules;
 	}
--- protected/modules/user/views/user/registration.php
+++ protected/modules/user/views/user/registration.php
@@ -78,18 +78,6 @@ $this->breadcrumbs=array(
 			}
 		}
 ?>
-	<?php if (UserModule::doCaptcha('registration')): ?>
-	<div class="row">
-		<?php echo $form->labelEx($model,'verifyCode'); ?>
-		
-		<?php $this->widget('CCaptcha'); ?>
-		<?php echo $form->textField($model,'verifyCode'); ?>
-		<?php echo $form->error($model,'verifyCode'); ?>
-		
-		<p class="hint"><?php echo UserModule::t("Please enter the letters as they are shown in the image above."); ?>
-		<br/><?php echo UserModule::t("Letters are not case-sensitive."); ?></p>
-	</div>
-	<?php endif; ?>
 	
 	<div class="row submit">
 		<?php echo CHtml::submitButton(UserModule::t("Register")); ?>
